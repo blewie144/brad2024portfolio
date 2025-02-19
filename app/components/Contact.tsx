d5ab015bbb338eda30ecdfc19cd88899bfa4ec5a
Bradley Lewis <launchpadweb@protonmail.com>
2025-02-19 13:27:08 +1030

    fixed error again

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -29,7 +29,7 @@ export default function Contact() {
         isError: false,
       });
       (e.target as HTMLFormElement).reset();
-    } catch (error: unknown) {
+    } catch (_error) {
       setSubmitStatus({
         message: 'Failed to send message. Please try again.',
         isError: true,
